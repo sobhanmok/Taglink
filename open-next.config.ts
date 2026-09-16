26572deda4a60fa32de71dd056caf9f9376a2f2d
Tag link <sobhan.dev@proton.me>
2026-09-16 02:43:35 -0700

    Simplify open-next config for new version

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,18 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
-import cache from "@opennextjs/cloudflare/kvCache";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => cache,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  // 👇 این خط مشکل رو حل می‌کنه
-  buildCommand: "next build",
-};
-
-export default config;
+export default defineCloudflareConfig();
